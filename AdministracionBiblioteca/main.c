f40610431f03c67bed7d5d0b6db66ea00956e694
Oscar <oscarcabrerarojas2023@gmail.com>
2024-11-07 19:12:47 -0600

    Preparación en VSCode y preparación de clase main

--- AdministracionBiblioteca/main.c
+++ AdministracionBiblioteca/main.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <bool.h>
+#include <stdbool.h>
+
+void printMenu()
+{
+    printf("\n\t¡Bienvenido al sistema!");
+    printf("\n\t¿Qué es lo que quieres hacer?");
+}
 
 int main()
 {
